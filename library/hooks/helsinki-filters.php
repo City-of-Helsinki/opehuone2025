0e2f0d3b0ab8b4e9aa709cc6e94945f14cf4f51e
Timo L <timo@luuptek.fi>
2025-03-02 20:06:28 +0200

    starting to build user profile

--- library/hooks/helsinki-filters.php
+++ library/hooks/helsinki-filters.php
@@ -14,3 +14,16 @@ add_filter('helsinki_header_search_enabled', '__return_true');
 add_filter('helsinki_wp_disallowed_blocks', function () {
 	return [];
 });
+
+// Remove topbar from header
+function remove_helsinki_topbar() {
+	if (has_action('helsinki_header_top', 'helsinki_topbar')) {
+		remove_action('helsinki_header_top', 'helsinki_topbar', 10);
+	}
+}
+add_action('wp_head', __NAMESPACE__ . '\\remove_helsinki_topbar');
+
+
+add_action('helsinki_header', function() {
+	get_template_part('partials/header/profile-opener');
+}, 41);
---
+++ partials/header/profile-opener.php
@@ -0,0 +1,66 @@
+<?php
+
+use Opehuone\Helpers;
+
+if ( ! is_user_logged_in() ) {
+	return;
+}
+
+$current_user = wp_get_current_user();
+
+// get current user first and last name: FL
+$first_name = $current_user->user_firstname;
+$last_name  = $current_user->user_lastname;
+
+// Get the first character of each name
+$first_initial = substr( $first_name, 0, 1 );
+$last_initial  = substr( $last_name, 0, 1 );
+
+// Combine initials
+$profile_name = strtoupper( $first_initial . $last_initial );
+?>
+<div class="profile-wrapper">
+	<button class="profile-opener" aria-expanded="false"
+			aria-label="<?php esc_attr_e( 'Avaa profiiliasetukset', 'helsinki-universal' ); ?>">
+		<span><?php echo esc_html( $profile_name ); ?></span>
+	</button>
+	<div class="profile-opener-dropdown">
+		<div class="profile-opener-dropdown__name-row">
+			<div class="profile-opener-dropdown__name-circle">
+				<?php echo esc_html( $profile_name ); ?>
+			</div>
+			<div>
+				<p class="profile-opener-dropdown__full-name">
+					<?php echo esc_html( $first_name . ' ' . $last_name ); ?>
+				</p>
+				<p class="profile-opener-dropdown__email">
+					<?php echo esc_html( $current_user->user_email ); ?>
+				</p>
+			</div>
+		</div>
+		<nav aria-label="<?php esc_attr_e( 'Profiiliasetusten navigointi', 'helsinki-universal' ); ?>">
+			<ul class="profile-opener-dropdown__links">
+				<li class="profile-opener-dropdown__link-item">
+					<a class="profile-opener-dropdown__link"
+					   href="<?php echo esc_url( get_permalink( get_field( 'user_settings_page', 'option' ) ) ); ?>">
+						<?php Helpers\the_svg( 'icons/face' ); ?>
+						<?php esc_html_e( 'Oma profiili', 'helsinki-universal' ); ?>
+					</a>
+				</li>
+				<li class="profile-opener-dropdown__link-item">
+					<a class="profile-opener-dropdown__link"
+					   href="<?php echo esc_url( get_permalink( get_field( 'user_settings_page', 'option' ) ) ); ?>">
+						<?php Helpers\the_svg( 'icons/envelope' ); ?>
+						<?php esc_html_e( 'Ota yhteyttä Opehuoneeseen', 'helsinki-universal' ); ?>
+					</a>
+				</li>
+				<li class="profile-opener-dropdown__link-item">
+					<a class="profile-opener-dropdown__link" href="<?php echo esc_url( wp_logout_url() ); ?>">
+						<?php Helpers\the_svg( 'icons/sign-out' ); ?>
+						<?php esc_html_e( 'Kirjaudu ulos', 'helsinki-universal' ); ?>
+					</a>
+				</li>
+			</ul>
+		</nav>
+	</div>
+</div>
