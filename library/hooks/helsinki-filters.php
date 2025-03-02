191e27d77821ea4419dd69cc8f4212f1c940dc9c
Timo L <timo@luuptek.fi>
2025-03-02 16:28:12 +0200

    make the content to show in editor, when content has been created with classic editor

--- library/hooks/helsinki-filters.php
+++ library/hooks/helsinki-filters.php
@@ -2,8 +2,15 @@
 
 namespace Opehuone\HelsinkiMainThemeFilters;
 
+// Default scheme
 add_filter( 'helsinki_default_scheme', function () {
 	return 'tram';
 } );
 
+// Enable search
 add_filter('helsinki_header_search_enabled', '__return_true');
+
+// Allow all blocks, especially classic (core/freeform) block
+add_filter('helsinki_wp_disallowed_blocks', function () {
+	return [];
+});
