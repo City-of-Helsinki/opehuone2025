2004121a716207290aecf721dae86d9096f52060
Didzis Zvaigzne <didzis.zvaigzne@hiq.fi>
2025-05-23 15:19:31 +0300

    Fix for PHP bugs where value is not checked for null. Saved work in progress.

--- partials/template-blocks/b-training-post.php
+++ partials/template-blocks/b-training-post.php
@@ -34,7 +34,7 @@ $type_svg      = TrainingHelpers::get_training_type_svg( $args['type'] );
 				<?php echo esc_html( $block_excerpt ); ?>
 			</p>
 		<?php endif; ?>
-		<?php if ( count( $block_categories ) > 0 ) : ?>
+		<?php if ( ! empty( $block_categories ) && count( $block_categories ) > 0 ) : ?>
 			<ul class="post-tags">
 				<?php
 				foreach ( $block_categories as $category ) {
