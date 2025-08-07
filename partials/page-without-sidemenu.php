65f5611548bc6e2a0618dd9abd8cca0b73342e2d
Henri Tikkanen <henri.tikkanen@ambientia.fi>
2025-08-07 12:26:11 +0300

    Minor style tweaks for pages

--- partials/page-without-sidemenu.php
+++ partials/page-without-sidemenu.php
@@ -1,10 +1,10 @@
 <article class="content">
-	<div class="content__container hds-container">
+	<div class="hds-container opehuone-page opehuone-content-container">
 		<?php get_template_part( 'partials/breadcrumbs' ); ?>
 		<div class="opehuone-grid">
 			<div>
-				<?php the_post_thumbnail( 'large', [ 'class' => 'single-post__featured-image' ] ); ?>
-				<h1 class="sidemenu-page-title"><?php the_title(); ?></h1>
+				<?php the_post_thumbnail( 'large', [ 'class' => 'featured-image' ] ); ?>
+				<h1 class="page-title"><?php the_title(); ?></h1>
 				<?php the_content(); ?>
 			</div>
 		</div>
