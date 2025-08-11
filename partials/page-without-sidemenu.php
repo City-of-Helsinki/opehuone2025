52d5626a4ccc3871961b0d7135bbe62bc219dee8
Henri Tikkanen <henri.tikkanen@ambientia.fi>
2025-08-11 16:49:42 +0300

    Minor style and template tweaks

--- partials/page-without-sidemenu.php
+++ partials/page-without-sidemenu.php
@@ -3,8 +3,14 @@
 		<?php get_template_part( 'partials/breadcrumbs' ); ?>
 		<div class="opehuone-grid">
 			<div>
-				<?php the_post_thumbnail( 'large', [ 'class' => 'featured-image' ] ); ?>
 				<h1 class="page-title"><?php the_title(); ?></h1>
+				<?php if ( has_excerpt() ) : ?>
+					<p class="single-post__excerpt">
+						<?php echo get_the_excerpt(); ?>
+					</p>
+				<?php endif; ?>
+				<?php get_template_part( 'partials/page-meta' );  ?>
+				<?php the_post_thumbnail( 'large', [ 'class' => 'featured-image' ] ); ?>
 				<?php the_content(); ?>
 			</div>
 		</div>
