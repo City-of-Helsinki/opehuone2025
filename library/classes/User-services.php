18539336d4fcbe7bc3204583cbf3f4c4ca092765
Didzis Zvaigzne <didzis.zvaigzne@hiq.fi>
2025-09-09 14:27:37 +0300

    if check to see if body is an array or a string

--- library/classes/User-services.php
+++ library/classes/User-services.php
@@ -44,7 +44,10 @@ class User_services {
         error_log( 'get_services_api_response' );
         error_log( json_encode( $api_response_body ) );
 
-        var_dump( $api_response_body );
+
+        if ( is_array( $api_response_body ) ) {
+            return $api_response_body;
+        }
 
         return json_decode( $api_response_body );
 //        if ( ! is_wp_error( $api_response_body ) ) {
