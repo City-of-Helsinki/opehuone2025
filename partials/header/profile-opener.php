1dba953360b93c56658fc1a3671ca4a1a6d4df3e
Didzis Zvaigzne <didzis.zvaigzne@hiq.fi>
2025-09-30 15:50:42 +0300

    Add wpo365OpenIdRedirect ID

--- partials/header/profile-opener.php
+++ partials/header/profile-opener.php
@@ -7,7 +7,7 @@ use function \Opehuone\TemplateFunctions\display_time_until_holidays;
 // Display the login button if the user is not logged in
 if ( ! is_user_logged_in() ) { ?>
 
-    <div class="login-wrapper">
+    <div id="wpo365OpenIdRedirect" class="login-wrapper">
         <a href="javascript:void(0)" onclick="window.wpo365.pintraRedirect.toMsOnline('', location.href, '', '', false, document.getElementById('selectedTenant') ? document.getElementById('selectedTenant').value : null)" class="login-button"
            title="<?php echo esc_html__('Kirjaudu edu.hel.fi' ); ?>">
 
