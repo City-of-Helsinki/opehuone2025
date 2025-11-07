44afcdd440199175c81f93f50a1d6a5ea7a44f63
Didzis Zvaigzne <didzis.zvaigzne@hiq.fi>
2025-11-07 13:54:58 +0200

    Remove autocomplete for user selection

--- resources/js/editor.js
+++ resources/js/editor.js
@@ -1,3 +1,11 @@
 import { registerTrainingSettings } from './settings/training';
 
 registerTrainingSettings();
+
+// Remove user autocomplete list from the editor
+wp.domReady(() => {
+	wp.hooks.removeFilter(
+		'editor.Autocomplete.completers',
+		'editor/autocompleters/set-default-completers'
+	);
+});
