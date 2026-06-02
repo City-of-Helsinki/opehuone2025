098e8b375bd84720bb4a4124ed7fdcf5f5721f54
GroteskiJasu <jasu@groteski.fi>
2026-06-02 14:19:05 +0300

    Build files

--- build/css/editor.asset.php
+++ build/css/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'ad9ef28328a6aaec79a2');
+<?php return array('dependencies' => array(), 'version' => '54ecbf89d55eb6f2c7b7');
--- build/css/screen.asset.php
+++ build/css/screen.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'c68b9b9aedba2fff55e3');
+<?php return array('dependencies' => array(), 'version' => '7953da492963781dff76');
