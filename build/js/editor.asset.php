f052a09409a92b6f2aa4a9ad99d169b791637f68
Kimmo Thai <kimmo.thai@hiq.fi>
2026-07-08 17:21:37 +0300

    Update package-lock.json to bump shell-quote from 1.8.2 to 1.9.0

--- build/js/editor.asset.php
+++ build/js/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-plugins', 'wp-primitives'), 'version' => '8c380b077c377872248b');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-plugins', 'wp-primitives'), 'version' => 'fde9127300a32f3c20c8');
