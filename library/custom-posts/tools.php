77cb14ef4ed5c3a8657df253b4763ea740bcd489
GroteskiJasu <jasu@groteski.fi>
2026-07-23 10:49:53 +0300

    Less duplication to file where new cpt for tools is registered (sonarcube bot analysis failed because of too much duplication?!?)

--- library/custom-posts/tools.php
+++ library/custom-posts/tools.php
@@ -7,38 +7,43 @@
  */
 function register_cpt_tools() {
 
+    $domain   = 'helsinki-universal';
+    $plural   = __( 'Omat työkalut', $domain );
+    $singular = __( 'Työkalu', $domain );
+
     $labels = array(
-        'name'                  => _x( 'Omat työkalut', 'Post Type yleinen nimi', 'helsinki-universal' ),
-        'singular_name'         => _x( 'Työkalu', 'Post Type yksittäinen nimi', 'helsinki-universal' ),
-        'menu_name'             => __( 'Omat työkalut', 'helsinki-universal' ),
-        'name_admin_bar'        => __( 'Työkalut', 'helsinki-universal' ),
-        'archives'              => __( 'Arkistot', 'helsinki-universal' ),
-        'attributes'            => __( 'Arkistot', 'helsinki-universal' ),
-        'parent_item_colon'     => __( 'Yläsivu:', 'helsinki-universal' ),
-        'all_items'             => __( 'Kaikki työkalut', 'helsinki-universal' ),
-        'add_new'               => __( 'Lisää uusi työkalu', 'helsinki-universal' ),
-        'new_item'              => __( 'Uusi työkalu', 'helsinki-universal' ),
-        'add_new_item'          => __( 'Lisää uusi työkalu', 'helsinki-universal' ),
-        'edit_item'             => __( 'Muokkaa työkalua', 'helsinki-universal' ),
-        'update_item'           => __( 'Päivitä työkalu', 'helsinki-universal' ),
-        'view_item'             => __( 'Katso työkalu', 'helsinki-universal' ),
-        'view_items'            => __( 'Katso työkalut', 'helsinki-universal' ),
-        'search_items'          => __( 'Etsi työkaluja', 'helsinki-universal' ),
-        'not_found'             => __( 'Ei löytynyt', 'helsinki-universal' ),
-        'not_found_in_trash'    => __( 'Ei löytynyt roskakorista', 'helsinki-universal' ),
-        'featured_image'        => __( 'Julkaisun kuva', 'helsinki-universal' ),
-        'set_featured_image'    => __( 'Aseta työkalun kuva', 'helsinki-universal' ),
-        'remove_featured_image' => __( 'Poista työkalun kuva', 'helsinki-universal' ),
-        'use_featured_image'    => __( 'Käytä työkalun kuvana', 'helsinki-universal' ),
-        'insert_into_item'      => __( 'Lisää työkaluun', 'helsinki-universal' ),
-        'uploaded_to_this_item' => __( 'Ladattu tähän työkaluun', 'helsinki-universal' ),
-        'items_list'            => __( 'Työkalujen listaus', 'helsinki-universal' ),
-        'items_list_navigation' => __( 'Työkalujen navigointi', 'helsinki-universal' ),
-        'filter_items_list'     => __( 'Suodata työkaluja', 'helsinki-universal' ),
+        'name'                  => _x( $plural, 'Post Type yleinen nimi', $domain ),
+        'singular_name'         => _x( $singular, 'Post Type yksittäinen nimi', $domain ),
+        'menu_name'             => $plural,
+        'name_admin_bar'        => __( 'Työkalut', $domain ),
+        'archives'              => __( 'Arkistot', $domain ),
+        'attributes'            => __( 'Arkistot', $domain ),
+        'parent_item_colon'     => __( 'Yläsivu:', $domain ),
+        'all_items'             => sprintf( __( 'Kaikki %s', $domain ), lcfirst( $plural ) ),
+        'add_new'               => sprintf( __( 'Lisää uusi %s', $domain ), lcfirst( $singular ) ),
+        'new_item'              => sprintf( __( 'Uusi %s', $domain ), lcfirst( $singular ) ),
+        'add_new_item'          => sprintf( __( 'Lisää uusi %s', $domain ), lcfirst( $singular ) ),
+        'edit_item'             => sprintf( __( 'Muokkaa %s', $domain ), lcfirst( $singular ) ),
+        'update_item'           => sprintf( __( 'Päivitä %s', $domain ), lcfirst( $singular ) ),
+        'view_item'             => sprintf( __( 'Katso %s', $domain ), lcfirst( $singular ) ),
+        'view_items'            => sprintf( __( 'Katso %s', $domain ), lcfirst( $plural ) ),
+        'search_items'          => sprintf( __( 'Etsi %s', $domain ), lcfirst( $plural ) ),
+        'not_found'             => __( 'Ei löytynyt', $domain ),
+        'not_found_in_trash'    => __( 'Ei löytynyt roskakorista', $domain ),
+        'featured_image'        => __( 'Julkaisun kuva', $domain ),
+        'set_featured_image'    => sprintf( __( 'Aseta %s kuva', $domain ), lcfirst( $singular ) ),
+        'remove_featured_image' => sprintf( __( 'Poista %s kuva', $domain ), lcfirst( $singular ) ),
+        'use_featured_image'    => sprintf( __( 'Käytä %s kuvana', $domain ), lcfirst( $singular ) ),
+        'insert_into_item'      => sprintf( __( 'Lisää %s', $domain ), lcfirst( $singular ) ),
+        'uploaded_to_this_item' => sprintf( __( 'Ladattu tähän %s', $domain ), lcfirst( $singular ) ),
+        'items_list'            => sprintf( __( '%s listaus', $domain ), $plural ),
+        'items_list_navigation' => sprintf( __( '%s navigointi', $domain ), $plural ),
+        'filter_items_list'     => sprintf( __( 'Suodata %s', $domain ), lcfirst( $plural ) ),
     );
-    $args   = array(
-        'label'               => __( 'Työkalut', 'helsinki-universal' ),
-        'description'         => __( 'Työkalut kuvaus', 'helsinki-universal' ),
+
+    $args = array(
+        'label'               => __( 'Työkalut', $domain ),
+        'description'         => __( 'Työkalut kuvaus', $domain ),
         'labels'              => $labels,
         'supports'            => array( 'title' ),
         'hierarchical'        => true,
@@ -55,6 +60,7 @@ function register_cpt_tools() {
         'show_in_rest'        => true,
         'menu_icon'           => 'dashicons-hammer',
     );
+
     register_post_type( 'tools', $args );
 }
 
